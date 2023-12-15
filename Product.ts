55f65ca822106d81cb2698bbdc88275ce165e9e0
Bishal Bhattarai <Bishal>
2023-12-15 12:50:17 +0545

    fix: product new item 5

--- Product.ts
+++ Product.ts
@@ -39,5 +39,13 @@ export const ProuductList: Product[] = [
         createdDate: new Date(),
         expiryDate: new Date(),
         price: 323
+    },
+    {
+        _key: '5',
+        name: 'Ice Cream5',
+        brand: 'brand2',
+        createdDate: new Date(),
+        expiryDate: new Date(),
+        price: 323 
     }
 ]
